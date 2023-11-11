beca1285dfd33c45dee2087b993b3479673ca5a2
sardor <Sardorshavkatillayev@gmail.com>
2023-11-11 12:31:56 +0500

    initial commit

---
+++ src/main/java/com/example/whiteudemyprod/WhiteUdemyProdApplication.java
@@ -0,0 +1,15 @@
+package com.example.whiteudemyprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteUdemyProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteUdemyProdApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/example/whiteudemyprod/WhiteUdemyProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whiteudemyprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteUdemyProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
