a4d85ca6ea4ec8599468a37a279515c520250823
took4a <zafarmalikov704@gmail.com>
2023-11-13 15:01:23 +0500

    User created with liquibase

--- src/main/java/com/example/whiteudemyprod/user/dto/UserCreateDto.java
+++ src/main/java/com/example/whiteudemyprod/user/dto/UserCreateDto.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 
 @Data
 public class UserCreateDto {
-    private UUID id;
     private String firstName;
     private String lastName;
     private String middleName;
--- src/main/java/com/example/whiteudemyprod/user/dto/UserUpdateDto.java
+++ src/main/java/com/example/whiteudemyprod/user/dto/UserUpdateDto.java
@@ -1,4 +1,15 @@
 package com.example.whiteudemyprod.user.dto;
 
+import com.example.whiteudemyprod.user.entity.UserType;
+import lombok.Data;
+
+@Data
 public class UserUpdateDto {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private UserType userType;
+    private String phoneNumber;
+    private String email;
+    private String password;
 }
